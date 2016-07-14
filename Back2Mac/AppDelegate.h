436df439a69aa35a653a9b3db83481829898017b
Seungho Kim <iolate@me.com>
2016-07-15 00:43:43 +0900

    Initial Commit

---
+++ Back2Mac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Back2Mac
+//
+//  Created by 승호 on 2016. 7. 15..
+//  Copyright © 2016년 iolate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
