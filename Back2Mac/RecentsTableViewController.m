e97d3f6e843112432b3547dfb80bc939bf31e8a7
Seungho Kim <iolate@me.com>
2016-07-18 00:16:54 +0900

    글 목록 - Pull to Refresh / 더보기

--- Back2Mac/RecentsTableViewController.m
+++ Back2Mac/RecentsTableViewController.m
@@ -15,31 +15,70 @@
 
 @interface RecentsTableViewController () {
     NSInteger defaultViewer;
+    BOOL pullToRefreshEnabled;
 }
-@property (nonatomic, strong) NSMutableArray* articleList;
+@property (nonatomic) NSInteger lastFetchedPage;
+@property (nonatomic, strong) NSArray* articleList;
 @property (nonatomic, strong) NSArray* readList;
 @property (nonatomic, strong) MBProgressHUD *hud;
+
 @end
 
 @implementation RecentsTableViewController
 
 -(void)errorAlertWithMessage:(NSString *)message {
-    dispatch_async(dispatch_get_main_queue(), ^() {
-        [self.hud hideAnimated:YES];
-        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Back2Mac"
-                                                                                 message:message
-                                                                          preferredStyle:UIAlertControllerStyleAlert];
-        
-        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"확인" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-            [self.navigationController popViewControllerAnimated:YES];
-        }];
-        [alertController addAction:cancelAction];
-        [self presentViewController:alertController animated:YES completion:nil];
-    });
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Back2Mac"
+                                                                             message:message
+                                                                      preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"확인" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }];
+    [alertController addAction:cancelAction];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
--(void)_fetchArticleListWithPage:(NSInteger)page {
-    NSString* url = [NSString stringWithFormat:@"http://%@/list", API_HOST];
+-(BOOL)addArticleWithDupCheckAndOrder:(NSArray *)newList {
+    if (self.articleList == nil || self.articleList.count == 0) {
+        _articleList = newList;
+        return TRUE;
+    }else{
+        NSMutableArray* orgIds = [NSMutableArray array];
+        for (NSDictionary* article in self.articleList) {
+            [orgIds addObject:article[@"id"]];
+        }
+        
+        NSMutableArray* newArticleList = [NSMutableArray array];
+        for (NSDictionary* article in newList) {
+            if ([orgIds containsObject:article[@"id"]]) {
+                continue;
+            }else{
+                [newArticleList addObject:article];
+            }
+        }
+        
+        if (newArticleList.count > 0) {
+            [newArticleList addObjectsFromArray:self.articleList];
+            
+            [newArticleList sortUsingComparator:^NSComparisonResult(NSDictionary* obj1, NSDictionary* obj2) {
+                NSInteger obj1Id = [obj1[@"id"] integerValue];
+                NSInteger obj2Id = [obj2[@"id"] integerValue];
+                
+                if (obj1Id < obj2Id) return NSOrderedDescending;
+                else return NSOrderedAscending;
+            }];
+            
+            self.articleList = [NSArray arrayWithArray:newArticleList];
+            
+            return TRUE;
+        }else{
+            return FALSE;
+        }
+    }
+}
+
+-(void)_fetchArticleListWithPage:(NSInteger)page completion:(void (^)(NSError* error))completionBlock {
+    NSString* url = [NSString stringWithFormat:@"http://%@/list?page=%ld", API_HOST, page];
     NSDictionary* api_data = [NSDictionary dictionaryWithContentsOfJSONURLString:url];
     
     if (api_data != nil) {
@@ -47,25 +86,31 @@
             if (self.articleList == nil) {
                 _articleList = [NSMutableArray array];
             }
+            BOOL articleWasAdded = [self addArticleWithDupCheckAndOrder:api_data[@"posts"]];
+            if (page > self.lastFetchedPage) self.lastFetchedPage = page;
             
-            [self.articleList addObjectsFromArray:api_data[@"posts"]];
-            
-            dispatch_async(dispatch_get_main_queue(), ^() {
-                [self.tableView reloadData];
-                [self.hud hideAnimated:YES];
-            });
-            return;
-        }else{
-            NSString* error = nil;
-            if ([api_data.allKeys containsObject:@"error"]) {
-                error = api_data[@"error"];
-            }else {
-                error = [NSString stringWithFormat:@"Error %@", api_data[@"result"]];
+            if (articleWasAdded) {
+                dispatch_async(dispatch_get_main_queue(), ^() {
+                    [self.tableView reloadData];
+                });
             }
-            [self errorAlertWithMessage:error];
+            
+            if (completionBlock != nil) completionBlock(nil);
+            return;
+        }else if (completionBlock != nil) {
+            NSString* errorMsg = nil;
+            if ([api_data.allKeys containsObject:@"error"]) {
+                errorMsg = api_data[@"error"];
+            }else {
+                errorMsg = [NSString stringWithFormat:@"Error %@", api_data[@"result"]];
+            }
+            
+            NSError* error = [NSError errorWithDomain:@"fetch" code:[api_data[@"result"] integerValue] userInfo:@{NSLocalizedDescriptionKey: errorMsg}];
+            completionBlock(error);
         }
-    }else{
-        [self errorAlertWithMessage:@"데이터 불러오기 실패"];
+    }else if (completionBlock != nil) {
+        NSError* error = [NSError errorWithDomain:@"fetch" code:-99 userInfo:@{NSLocalizedDescriptionKey: @"데이터 불러오기 실패"}];
+        completionBlock(error);
     }
 }
 
@@ -75,13 +120,20 @@
         [self.navigationController.view addSubview:self.hud];
     }
     [self.hud showAnimated:YES];
+    
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        [self _fetchArticleListWithPage:1];
+        [self _fetchArticleListWithPage:page completion:^(NSError *error) {
+            dispatch_async(dispatch_get_main_queue(), ^() {
+                if (self.hud != nil) [self.hud hideAnimated:YES];
+                if (error != nil) [self errorAlertWithMessage:[error localizedDescription]];
+            });
+        }];
     });
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.lastFetchedPage = 0;
     [self fetchArticleListWithPage:1];
 }
 
@@ -107,6 +159,17 @@
     [super didReceiveMemoryWarning];
 }
 
+#pragma mark -
+
+-(IBAction)refreshTable:(id)sender {
+    [self _fetchArticleListWithPage:1 completion:^(NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^() {
+            [self.refreshControl endRefreshing];
+            if (error != nil) [self errorAlertWithMessage:[error localizedDescription]];
+        });
+    }];
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -114,37 +177,52 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.articleList count];
+    return [self.articleList count] > 0 ? [self.articleList count]+1 : 0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return (indexPath.row == [self.articleList count]) ? 44.0f : 77.0f;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSDictionary* post = self.articleList[indexPath.row];
-    ArticleListTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[post.allKeys containsObject:@"thumbnail"] ? @"Cell" : @"CellNoImage" forIndexPath:indexPath];
-    if ([post.allKeys containsObject:@"thumbnail"]) {
-        [cell.aImageView sd_setImageWithURL:[NSURL URLWithString:post[@"thumbnail"]] placeholderImage:nil];
+    if (indexPath.row == self.articleList.count) {
+        UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"CellLoadMore" forIndexPath:indexPath];
+        
+        return cell;
+    }else{
+        NSDictionary* post = self.articleList[indexPath.row];
+        ArticleListTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[post.allKeys containsObject:@"thumbnail"] ? @"Cell" : @"CellNoImage" forIndexPath:indexPath];
+        if ([post.allKeys containsObject:@"thumbnail"]) {
+            [cell.aImageView sd_setImageWithURL:[NSURL URLWithString:post[@"thumbnail"]] placeholderImage:nil];
+        }
+        
+        cell.aTitle.text = post[@"title"];
+        cell.aCategory.text = post[@"category"];
+        cell.aTime.text = post[@"date"];
+        
+        [cell isRead:[self.readList containsObject:post[@"id"]]];
+        
+        return cell;
     }
-    
-    cell.aTitle.text = post[@"title"];
-    cell.aCategory.text = post[@"category"];
-    cell.aTime.text = post[@"date"];
-    
-    [cell isRead:[self.readList containsObject:post[@"id"]]];
-    
-    return cell;
 }
 
 #pragma mark - Table view delegate
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (defaultViewer == 2) {
-        NSString* articleId = self.articleList[indexPath.row][@"id"];
-        [Back2Mac articleId:articleId toRead:YES];
-        [[UIApplication sharedApplication] openURL:[Back2Mac getURL:articleId]];
+    if (indexPath.row == self.articleList.count) {
+        [self fetchArticleListWithPage:self.lastFetchedPage+1];
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
     }else{
-        [self performSegueWithIdentifier:@"webViewSegue" sender:self];
+        if (defaultViewer == 2) {
+            NSString* articleId = self.articleList[indexPath.row][@"id"];
+            [Back2Mac articleId:articleId toRead:YES];
+            [[UIApplication sharedApplication] openURL:[Back2Mac getURL:articleId]];
+        }else{
+            [self performSegueWithIdentifier:@"webViewSegue" sender:self];
+        }
+        
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
     }
-    
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 
